d565df045b890daf56a6fec5d85c3dba04a6dfb7
Junichi Ito <jit@sonicgarden.jp>
2013-03-09 15:22:50 +0900

    Accept length option

--- spec/dummy_text_jp_spec.rb
+++ spec/dummy_text_jp_spec.rb
@@ -4,5 +4,9 @@ require File.expand_path('spec_helper', File.dirname(__FILE__))
 describe DummyTextJp do
   describe "#text" do
     specify { DummyTextJp.text.length.should > 0 }
+
+    context 'when with argument' do
+      specify { DummyTextJp.text(10).length.should == 10 }
+    end
   end
 end
