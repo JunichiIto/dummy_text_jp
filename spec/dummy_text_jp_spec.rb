2f4b804519b2389309a9184b15321f5672d4a900
Junichi Ito <jit@sonicgarden.jp>
2013-05-21 21:10:27 +0900

    Change texts randomly

--- spec/dummy_text_jp_spec.rb
+++ spec/dummy_text_jp_spec.rb
@@ -2,11 +2,19 @@
 require File.expand_path('spec_helper', File.dirname(__FILE__))
 
 describe DummyTextJp do
-  describe "#text" do
+  describe "::text" do
     specify { DummyTextJp.text.length.should > 0 }
 
     context 'when with argument' do
       specify { DummyTextJp.text(10).length.should == 10 }
+    end
+
+    it 'changes randomly' do
+      results = []
+      10.times do
+        results << DummyTextJp.text(10)
+      end
+      expect(results.uniq.size).to be > 1
     end
   end
 end
